ce0b6158973ee4c87c9a412465d56ecb740bd5d8
Harshita Bhadri <harshitabhadri107@gmail.com>
2026-09-24 14:59:25 +0530

    save the modified

--- average.py
+++ average.py
@@ -1,6 +1,8 @@
 a=int(input("enter the first number:"));
+print("hi welcome!")
 b=int(input("enter the second number:"));
 c=int(input("enter the third number:"));
 p=int(input("enter the fourth number:"));
 d=a+b+c+p/4
-print("Average of three numbers is:");
+print("Average of three numbers is:",d);
+print("thank you!");
